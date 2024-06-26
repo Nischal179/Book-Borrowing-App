55f2c252b5e7f2691f04a52d9470f895f5d9c953
Nischal876 <nischalaryal11@gmail.com>
2024-06-26 23:47:18 +0545

    LibraryUserRepository was created

--- src/main/java/com/nischal/book_borrowing_app/repository/LibraryUserRepository.java
+++ src/main/java/com/nischal/book_borrowing_app/repository/LibraryUserRepository.java
@@ -1,4 +1,11 @@
 package com.nischal.book_borrowing_app.repository;
 
-public interface LibraryUserRepository {
+import com.nischal.book_borrowing_app.model.LibraryUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LibraryUserRepository extends JpaRepository<LibraryUser, Integer> {
+
+    Optional<LibraryUser> findByUsername(String username);
 }
