58c31c3beae2aa4d6c767985c8c719c6a861eaed
Nischal876 <nischalaryal11@gmail.com>
2024-06-26 16:27:59 +0545

    Project initiated and connected with postgreSQL database

---
+++ src/main/java/com/nischal/book_borrowing_app/BookBorrowingAppApplication.java
@@ -0,0 +1,13 @@
+package com.nischal.book_borrowing_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBorrowingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBorrowingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nischal/book_borrowing_app/BookBorrowingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nischal.book_borrowing_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBorrowingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
