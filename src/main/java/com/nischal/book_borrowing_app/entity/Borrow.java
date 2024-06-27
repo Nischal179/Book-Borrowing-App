e24a4cf3785b0f450d1d567b63e178a1cf2691f1
Nischal876 <nischalaryal11@gmail.com>
2024-06-27 14:34:38 +0545

    create Borrow class that creates borrow entity in the database

---
+++ src/main/java/com/nischal/book_borrowing_app/entity/Borrow.java
@@ -0,0 +1,31 @@
+package com.nischal.book_borrowing_app.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+public class Borrow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer borrowID;
+
+    @ManyToOne
+    @JoinColumn(name = "borrowerID")
+    private Borrower borrower;
+
+    @ManyToOne
+    @JoinColumn(name = "bookID")
+    private Book book;
+
+    private LocalDate borrowDate;
+
+
+}
