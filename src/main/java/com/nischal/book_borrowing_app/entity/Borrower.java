1bf198ef05a3b23015d2b71d1fe735a4c1c29d6c
Nischal876 <nischalaryal11@gmail.com>
2024-06-27 14:19:09 +0545

    Coded Borrower.java to create borrower entity

--- src/main/java/com/nischal/book_borrowing_app/entity/Borrower.java
+++ src/main/java/com/nischal/book_borrowing_app/entity/Borrower.java
@@ -1,4 +1,51 @@
 package com.nischal.book_borrowing_app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Borrower {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer borrowerID;
+    private String address;
+    private String mobileNo;
+    private String email;
+
+    public Integer getBorrowerID() {
+        return borrowerID;
+    }
+
+    public void setBorrowerID(Integer borrowerID) {
+        this.borrowerID = borrowerID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
