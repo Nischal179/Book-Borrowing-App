02f8948cb169dd53f82e51d0cebb3f60bdfb2e78
Nischal876 <nischalaryal11@gmail.com>
2024-06-27 09:47:52 +0545

    Lambok and JWT dependency added

--- src/main/java/com/nischal/book_borrowing_app/service/UserDetailsServiceImp.java
+++ src/main/java/com/nischal/book_borrowing_app/service/UserDetailsServiceImp.java
@@ -1,10 +1,20 @@
 package com.nischal.book_borrowing_app.service;
 
+import com.nischal.book_borrowing_app.repository.LibraryUserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImp implements UserDetailsService {
+
+
+    private final LibraryUserRepository repository;
+
+    public UserDetailsServiceImp(LibraryUserRepository repository) {
+        this.repository = repository;
+    }
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
