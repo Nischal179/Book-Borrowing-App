6a1df36f616fa19c1105261df7faa88f088c373a
Nischal876 <nischalaryal11@gmail.com>
2024-06-27 13:39:47 +0545

    Class to create Book entity added

---
+++ src/main/java/com/nischal/book_borrowing_app/entity/Book.java
@@ -0,0 +1,68 @@
+package com.nischal.book_borrowing_app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bookId;
+
+    private String bookName;
+    private String author;
+    private Double price;
+    private Integer quantity;
+    private Boolean availableStatus;
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Boolean getAvailableStatus() {
+        return availableStatus;
+    }
+
+    public void setAvailableStatus(Boolean availableStatus) {
+        this.availableStatus = availableStatus;
+    }
+}
