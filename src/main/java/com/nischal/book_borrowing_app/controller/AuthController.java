8ba38b47efd0930bae409d567e878a1146084292
Nischal876 <nischalaryal11@gmail.com>
2024-06-28 11:20:25 +0545

    create AuthResponse method inside AuthController class to handel authentication request and display message accordingly

--- src/main/java/com/nischal/book_borrowing_app/controller/AuthController.java
+++ src/main/java/com/nischal/book_borrowing_app/controller/AuthController.java
@@ -1,6 +1,34 @@
 package com.nischal.book_borrowing_app.controller;
 
+import com.nischal.book_borrowing_app.dto.AuthRequest;
+import com.nischal.book_borrowing_app.dto.AuthResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest authRequest) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+        } catch (AuthenticationException e) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return new AuthResponse("Login successful");
+    }
 }
