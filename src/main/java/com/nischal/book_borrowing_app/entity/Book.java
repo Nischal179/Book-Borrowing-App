8daa7e3ef9e0d7ce9c8398449ff681b33933d5c8
Nischal876 <nischalaryal11@gmail.com>
2024-07-14 10:33:34 +0545

    Add conditions for data entry in Book class

--- src/main/java/com/nischal/book_borrowing_app/entity/Book.java
+++ src/main/java/com/nischal/book_borrowing_app/entity/Book.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,10 +18,22 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer bookId;
+
+    @NotBlank(message = "Title is mandatory")
     private String bookName;
+
+    @NotBlank(message = "Author name is mandatory")
     private String author;
+
+    @NotNull(message = "Price is mandatory")
+    @Min(value = 0, message = "Price cannot be negative")
     private Double price;
+
+    @NotNull(message = "Quantity is mandatory")
+    @Min(value = 0, message = "Quantity cannot be negative")
     private Integer quantity;
+
+
     private Boolean availableStatus;
 
 }
