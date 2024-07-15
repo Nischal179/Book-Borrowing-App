7b06a54be93d385fbd93368b904d724ba31148e1
Nischal876 <nischalaryal11@gmail.com>
2024-07-15 11:35:37 +0545

    Add Exception util class that is called to throw custom message based on the exception type and removes the redundency of creating new object for throwing exception in each PUT, POST and DELETE request of each controller

---
+++ src/main/java/com/nischal/book_borrowing_app/util/ExceptionUtil.java
@@ -0,0 +1,18 @@
+package com.nischal.book_borrowing_app.util;
+
+import com.nischal.book_borrowing_app.customError.CustomException;
+
+public class ExceptionUtil {
+
+    public static void handleException(String id, Exception e)
+    {
+        if (e instanceof NumberFormatException) {
+            throw new IllegalArgumentException("Bad Request: ID must be a number:" + id);
+        } else if (e instanceof CustomException) {
+            throw new IllegalArgumentException("Not Found: Data for corresponding id: " + id);
+        }
+        else {
+            throw new IllegalArgumentException("Bad Request");
+        }
+    }
+}
