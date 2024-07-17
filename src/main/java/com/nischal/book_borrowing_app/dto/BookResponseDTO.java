7bbdc3014edc66e5755aef325fe026dd9944c6e7
Nischal876 <nischalaryal11@gmail.com>
2024-07-17 15:46:30 +0545

    Add a BookResponseDTO for response in GET methods

--- src/main/java/com/nischal/book_borrowing_app/dto/BookResponseDTO.java
+++ src/main/java/com/nischal/book_borrowing_app/dto/BookResponseDTO.java
@@ -1,17 +1,16 @@
 package com.nischal.book_borrowing_app.dto;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class BookRequestDTO {
+public class BookResponseDTO {
 
-    //For request bodies in POST and PUT methods
+    //For response in GET method
     private String bookName;
     private String author;
     private Double price;
-    private boolean availableStatus;
     private Integer quantity;
+    private boolean availableStatus;
 }
