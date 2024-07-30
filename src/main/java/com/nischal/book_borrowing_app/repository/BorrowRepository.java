ab0ced2edc61f28a23c04beed028b0229f0be992
Nischal876 <nischalaryal11@gmail.com>
2024-07-30 10:40:35 +0545

    Implemented logic while deleting book record -All borrow record associated with book where return status is true gets deleted first - Then book record associated with the provided id will be deleted - This ensures that foreign key constraint wont be violated

--- src/main/java/com/nischal/book_borrowing_app/repository/BorrowRepository.java
+++ src/main/java/com/nischal/book_borrowing_app/repository/BorrowRepository.java
@@ -14,4 +14,7 @@ public interface BorrowRepository extends JpaRepository<Borrow,Integer> {
     List<Borrow> findByBorrowerId(Integer borrowerId);
     @Query("SELECT b FROM Borrow b WHERE b.book.id = :bookId AND b.returnStatus = false")
     List<Borrow> findByBookId(Integer bookId);
+
+    @Query("SELECT b FROM Borrow b WHERE b.book.id = :bookId AND b.returnStatus = true")
+    List<Borrow> findByBookIdAndIsReturnedTrue(Integer bookId);
 }
