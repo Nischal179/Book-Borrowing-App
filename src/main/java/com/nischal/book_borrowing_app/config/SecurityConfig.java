203d412ab4c38c5222172f31e349932619fb642a
Nischal876 <nischalaryal11@gmail.com>
2024-09-02 22:08:07 +0545

    Add AuthenticationManager bean using AuthenticationConfiguration

--- src/main/java/com/nischal/book_borrowing_app/config/SecurityConfig.java
+++ src/main/java/com/nischal/book_borrowing_app/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.nischal.book_borrowing_app.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,4 +48,9 @@ public class SecurityConfig {
         return provider;
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 }
