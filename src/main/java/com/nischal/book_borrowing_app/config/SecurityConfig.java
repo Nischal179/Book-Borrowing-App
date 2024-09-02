fab66bde0386a16a0774e71c61b9a35ac0a00c21
Nischal876 <nischalaryal11@gmail.com>
2024-09-02 22:00:34 +0545

    Add AuthenticationProvider bean with BCrypt encryption and custom UserDetailsService

--- src/main/java/com/nischal/book_borrowing_app/config/SecurityConfig.java
+++ src/main/java/com/nischal/book_borrowing_app/config/SecurityConfig.java
@@ -3,14 +3,21 @@ package com.nischal.book_borrowing_app.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -24,6 +31,19 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    // Using our own authentication provider to customize the authentication process
+    // Providing control over password encoding amd user detail retrieval essential for
+    // scenario like integrating with a custom database
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
     }
 
 }
