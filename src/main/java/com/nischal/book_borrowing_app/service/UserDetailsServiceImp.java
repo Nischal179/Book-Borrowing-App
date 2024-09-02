4fc54bd46c875cb076e0a23a367e80ced33d05ef
Nischal876 <nischalaryal11@gmail.com>
2024-09-02 21:45:54 +0545

    Handle UserPrincipal creation and throw exception if user not found

--- src/main/java/com/nischal/book_borrowing_app/service/UserDetailsServiceImp.java
+++ src/main/java/com/nischal/book_borrowing_app/service/UserDetailsServiceImp.java
@@ -1,22 +1,29 @@
 package com.nischal.book_borrowing_app.service;
 
+import com.nischal.book_borrowing_app.entity.LibraryUser;
 import com.nischal.book_borrowing_app.repository.LibraryUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
+
 public class UserDetailsServiceImp implements UserDetailsService {
 
-
-    private final LibraryUserRepository repository;
-
-    public UserDetailsServiceImp(LibraryUserRepository repository) {
-        this.repository = repository;
-    }
-
+    @Autowired
+    private LibraryUserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        Optional<LibraryUser> users = userRepository.findByUsername(username);
+
+        if (users == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+//  DaoAuthenticationProvider expects a UserDetails object to represent
+//  the authenticated user, and UserPrincipal meets this requirement.
+        return new UserPrincipal(users);
     }
 }
