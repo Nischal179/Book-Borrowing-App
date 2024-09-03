5191f3392db55d0c9caf556e57cb60883095aa27
Nischal876 <nischalaryal11@gmail.com>
2024-09-03 12:07:50 +0545

    Add access token validation from cookies in JWT filter
    - Retrieve and validate access token from cookies
    - Check token expiration and log the status
    - Handle cases where the token is null, malformed, or expired

--- src/main/java/com/nischal/book_borrowing_app/filter/JwtFilter.java
+++ src/main/java/com/nischal/book_borrowing_app/filter/JwtFilter.java
@@ -1,13 +1,17 @@
 package com.nischal.book_borrowing_app.filter;
 
+import com.nischal.book_borrowing_app.service.JwtService;
 import com.nischal.book_borrowing_app.service.UserDetailsServiceImp;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,8 +25,44 @@ public class JwtFilter extends OncePerRequestFilter {
     @Autowired
     UserDetailsServiceImp userDetailsServiceImp;
 
+    @Autowired
+    private JwtService jwtService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Get access token from cookies
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            String accessToken = getTokenFromCookies(request, "access_token");
 
+            logger.info("Access Token: " + accessToken);
+
+            try {
+                if (accessToken != null && accessToken.contains(".")) {
+                    // Check if the access token is expired
+                    if (jwtService.isTokenExpired(accessToken)) {
+                        logger.info("Access token expired.");
+                    } else {
+                        logger.info("Access token is still valid.");
+                    }
+                } else {
+                    logger.warn("Access token is null or malformed.");
+                }
+            } catch (ExpiredJwtException e) {
+                logger.error("Access token expired and cannot be parsed.");
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private String getTokenFromCookies(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
     }
 }
